2ade9d6cc4a1d7fd3d5e7437fcaeedc790044756
Luke Badgerow <badgerow.luke@gmail.com>
2021-03-05 12:16:07 -0500

    to be able to toggle between tracking and not by environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 import { BootstrapConfig } from './app.config';
 import { EnvironmentService } from './services/environment/environment.service';
 import { LoggingService } from './services/monitoring/monitoring.service'
 
-export function initAuth(config: BootstrapConfig,  env:EnvironmentService) {
+export function initAuth(config: BootstrapConfig,  envService:EnvironmentService) {
   const appInsights = new ApplicationInsights({
     config: {
         instrumentationKey: config.insights.instrumentationKey,
-        enableAutoRouteTracking: true // option to log all route changes
+        enableAutoRouteTracking: environment.production // option to log all route changes
     }
   });
   appInsights.loadAppInsights()
   appInsights.trackPageView();
-  env.setEnvironment(config)
+  envService.setEnvironment(config)
   EnvironmentService.monitor = appInsights;
   return () => {}
   
